2204c35807854e5a8f0ea68519ff6509840d0406
Luke256 <55425834+Luke256@users.noreply.github.com>
2021-03-01 12:54:01 +0900

    Update school.js

--- CPP/school.js
+++ CPP/school.js
@@ -1,4 +1,4 @@
 window.onload = function(){
   var name=document.getElementById("luke");
-  name.setAttribute("style", "color:blue; font-size:500px; border:#f00 5px solid");
+  name.setAttribute("style", "color:blue; font-size:100px; border:#f00 5px solid; background-color:lightblue;");
 }
